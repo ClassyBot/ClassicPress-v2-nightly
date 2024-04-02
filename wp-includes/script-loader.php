926f3466118ed222ca934ff3575fc01c1ebe0acb
ClassyBot <bots@classicpress.net>
2024-04-02 00:04:19 +0000

    Nightly build 2.0.0+nightly.20240401

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -53,7 +53,7 @@ function classicpress_asset_version( $type = 'script', $handle = null ) {
 	static $default_version;
 
 	if ( empty( $default_version ) ) {
-		$default_version = 'cp_595a3314';
+		$default_version = 'cp_d9e2b919';
 	}
 
 	/**
