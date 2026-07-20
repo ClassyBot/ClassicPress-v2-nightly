a54a61232019f2283c21ad449420f0efe7ec0da0
ClassyBot <bots@classicpress.net>
2026-07-20 00:03:50 +0000

    Nightly migration build 2.7.0+migration.20260719

--- wordpress/wp-includes/script-loader.php
+++ wordpress/wp-includes/script-loader.php
@@ -53,7 +53,7 @@ function classicpress_asset_version( $type = 'script', $handle = null ) {
 	static $default_version;
 
 	if ( empty( $default_version ) ) {
-		$default_version = 'cp_b448c12e';
+		$default_version = 'cp_e267dde1';
 	}
 
 	/**
