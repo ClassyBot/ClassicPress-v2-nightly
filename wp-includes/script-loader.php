f4a63910dbcc9165a4ad89b37ac7ce152416d485
ClassyBot <bots@classicpress.net>
2026-08-20 00:04:34 +0000

    Nightly build 2.7.1+nightly.20260819

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -53,7 +53,7 @@ function classicpress_asset_version( $type = 'script', $handle = null ) {
 	static $default_version;
 
 	if ( empty( $default_version ) ) {
-		$default_version = 'cp_77c9ad46';
+		$default_version = 'cp_632794ff';
 	}
 
 	/**
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -27,7 +27,7 @@
  *
  * @global string $cp_version
  */
-$cp_version = '2.7.1+nightly.20260818';
+$cp_version = '2.7.1+nightly.20260819';
 
 /**
  * The WordPress version string.
